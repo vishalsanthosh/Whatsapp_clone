b4bafca59756bcdfd2442143fac7f4bd4c0f8e0d
aleenakrishna <aleenakrish59@gmail.com>
2024-09-18 13:22:23 +0530

    hi

---
+++ lib/database.dart
@@ -0,0 +1,75 @@
+class Database {
+  static List<Map> listIm=[
+  {
+  'i1':'assets/images/image1.jpg',
+  'n1':'Sanjan',
+  't1':"evdaaa?",
+  't2':'just now'
+
+  },
+  {
+  'i1':'assets/images/2.jpg',
+  'n1':'Vipin',
+  't1':"Purathott pokam ba",
+  't2':'12:36pm'
+
+  },
+  {
+  'i1':'assets/images/3.jpg',
+  'n1':'Mayan',
+  't1':"Daaaaa!",
+  't2':'12:00pm'
+
+  },
+  {
+  'i1':'assets/images/4.jpg',
+  'n1':'Amal',
+  't1':"Hi",
+  't2':'Yesterday'
+
+  },
+  {
+  'i1':'assets/images/5.jpg',
+  'n1':'Akhil',
+  't1':"daa nalle Freeyano?",
+  't2':'Yesterday'
+
+  },
+  {
+  'i1':'assets/images/6.jpg',
+  'n1':'Mom',
+  't1':"Gud mrng",
+  't2':'yesterday'
+
+  },
+  {
+  'i1':'assets/images/7.jpg',
+  'n1':'+9053654189',
+  't1':"Hi",
+  't2':'17/09/2024'
+
+  },
+  {
+  'i1':'assets/images/8.jpg',
+  'n1':'+8096354125',
+  't1':"is this Vishal?",
+  't2':'17/09/2024'
+
+  },
+  {
+  'i1':'assets/images/9.jpg',
+  'n1':'Shajina',
+  't1':"Da",
+  't2':'16/09/2024'
+
+  },
+  {
+  'i1':'assets/images/10.jpg',
+  'n1':'Soorya',
+  't1':"Hi",
+  't2':'16/09/2024'
+
+  },
+  
+  ];
+}
---
+++ lib/homescreen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/database.dart';
+
+class Homescreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+  /*Future.delayed(Duration(seconds: 5),(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context)=> UpdateScreen()),
+    );
+  });*/
+  return Scaffold(
+backgroundColor: Colors.black,
+appBar: AppBar(
+  backgroundColor: Colors.black,
+  title: Text('WhatsApp',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
+  actions: [
+    IconButton(onPressed: (){}, icon: Icon(Icons.qr_code_scanner,size: 20,color: Colors.white,)),
+    IconButton(onPressed: (){}, icon: Icon(Icons.camera_alt_outlined,size: 20,color: Colors.white,)),
+    IconButton(onPressed: (){}, icon: Icon(Icons.more_vert,size: 20,color: Colors.white,))
+  ],
+),
+body: Column(
+  children: [
+    Container(
+      height: 50,
+      width: double.infinity,
+      
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(27),color: Colors.grey),
+      child: Padding(padding: EdgeInsets.all(13),
+      child: Text("Search....",style: TextStyle(color: Colors.white,fontSize: 15),),),
+    ),
+    Expanded(child: ListView.builder(itemCount:10,itemBuilder: (context, index) {
+      return ListTile(
+        leading: CircleAvatar(
+          radius: 18,
+          foregroundImage:AssetImage(Database.listIm[index]['i1']) ,
+        ),
+        title: Text(Database.listIm[index]['n1'],style: TextStyle(color: Colors.white),),
+        subtitle: Text(Database.listIm[index]['t1'],style: TextStyle(color: Colors.grey,fontSize: 13),),
+        trailing: Text(Database.listIm[index]['t2'],style: TextStyle(color: Colors.grey),),
+      );
+    },)),
+    Padding(padding: EdgeInsets.all(10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),color: const Color.fromARGB(255, 53, 228, 59),
+          ),
+          child: Icon(Icons.add_comment_rounded,color: Colors.black,),
+        ),
+      ],
+    ),
+    ),
+  ],
+),
+bottomNavigationBar: BottomNavigationBar(
+  backgroundColor:  Colors.green,
+  selectedItemColor: Colors.green,
+  unselectedItemColor: Colors.black,
+  items:[
+    BottomNavigationBarItem(icon: Icon(Icons.message_outlined,color: Colors.black,),label: "Chats"),
+    BottomNavigationBarItem(icon: Icon(Icons.blur_circular_rounded,color:),label: "Updates"),
+    BottomNavigationBarItem(icon: Icon(Icons.groups,),label: "Communites"),
+    BottomNavigationBarItem(icon: Icon(Icons.call_sharp),label: "Calls"),
+  ] 
+),
+
+
+
+
+  );
+  }
+}
